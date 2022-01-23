f64daf804fff03e1f8d32a16a77c52f67ecf6b61
cmcguigan1 <mcguigan.claire@outlook.com>
2022-01-23 16:37:13 -0500

    Adding the edit todo functionality

--- src/index.js
+++ src/index.js
@@ -1,43 +1,22 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import { useState, useReducer } from 'react';
+import { useState, useReducer, useRef } from 'react';
 import './index.css' ;
 
-/*function List() {
-
-    return (
-        <>
-        <h1>Claire's ToDo List</h1>
-        <ul>
-            <li key="one">Todo 1:</li>
-        </ul>
-        <button onClick={() => Add()}>+</button>
-        </>
-    )
-}
-
-function Add(){
-    const [counter, setCounter] = useState(1);
-    const [todo, setTodo] = useState("");
-
-    //useEffect( () => { todo = [...todo, todo], [todo]);
-
-    return (
-        <li key={counter}>Todo {counter}:</li>
-    );
-}*/
 const initial = [
     {
       id: 1,
-      title: "Todo 1 : ",
+      title: "ToDo 1 : ",
       complete: false,
       editMode: false,
+      textValue: "Click here to edit",
     },
     {
       id: 2,
-      title: "Todo 2 : ",
+      title: "ToDo 2 : ",
       complete: false,
       editMode: false,
+      textValue: "Click here to edit",
     },
 ];
 
@@ -57,27 +36,29 @@ const reducer = (state, action) => {
             const newNum = lastTodo.id + 1; 
             const newTodo = {
                 id: newNum, 
-                title: "Todo " + newNum + " : ",
+                title: "ToDo " + newNum + " : ",
                 complete: false,
                 editMode: false,
+                textValue: "Click here to edit",
             }
             //console.log(...state,newTodo);
             const newState = [...state,newTodo]
             return newState;   
         case "EDIT":
-            console.log(action.id);
+            //console.log(action.id);
             return state.map((todo) => {
                 if (todo.id === action.id) {
-                    return { ...todo, editMode: !todo.editMode };
+                    return { ...todo, editMode: !todo.editMode};
                 } else {
                     return todo;
             }
             });
         case "SAVE":
-            console.log(action.id);
+            //console.log(action.id);
+            //console.log(action.textValue);
             return state.map((todo) => {
                 if (todo.id === action.id) {
-                    return { ...todo, hidden: true };
+                    return { ...todo, textValue: action.textValue};
                 } else {
                     return todo;
             }
@@ -90,36 +71,45 @@ const reducer = (state, action) => {
 
 function Todo(){
 const [todos, dispatch] = useReducer(reducer, initial);
+const [inputValue, setInputValue] = useState("Click here to edit");
 
 const handleCompleted = (todo) => { dispatch({ type: "COMPLETE", id: todo.id }); };
 const handleAdd = () => { dispatch({ type: "ADD"}); };
 const handleEdit = (todo) => { dispatch( { type: "EDIT", id: todo.id})};
-const handleSave = (todo) => { dispatch( { type: "SAVE", id: todo.id})};
+const handleSave = (todo,text) => { dispatch( { type: "SAVE", id: todo.id, textValue: text})};
 
 return(
     <>
         <h1>Claire's ToDo List</h1>
         <div id="todoList" >
+        <div id="values" >
         {todos.map((todo) => (
             <div key={todo.id}>
-            <label>
-                <input
-                    type="checkbox"
-                    checked={todo.complete}
-                    onChange={() => handleCompleted(todo)}
-                />
-            {todo.title}
-            </label>
             <label onDoubleClick={() => handleEdit(todo)}>
-                <input id="editBox"
-                    type= "text"
-                    hidden= {!todo.editMode}
-                />
-                <button hidden={true} onClick={() => handleSave(todo)}>Save</button>
-            Click here to edit
+                <label>
+                    <input
+                        type="checkbox"
+                        checked={todo.complete}
+                        onChange={() => handleCompleted(todo)}
+                    />
+                {todo.title}
+                </label>
+                <label >
+                    <input id="editBox"
+                        type= "text"
+                        //value = {inputValue}
+                        hidden= {!todo.editMode}
+                        //onChange={(e) => setInputValue(e.target.value)}
+                        value= {todo.textValue}
+                        onChange={(e) => handleSave(todo,e.target.value)}
+                    />
+                    <button hidden={!todo.editMode} onClick={() => handleEdit(todo)}>Save</button>
+                {!todo.editMode ? todo.textValue : ""}
+                </label>
             </label>
-        </div>
+            </div>
         ))}
+        </div>
         <button id="add" onClick={handleAdd}>+</button>
         </div>
     </>
