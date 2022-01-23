a43461aaeac85b76f36ce7f485e5b50849fc0f55
cmcguigan1 <mcguigan.claire@outlook.com>
2022-01-23 02:10:36 -0500

    Adding some CSS and a bit more functionality

--- src/index.js
+++ src/index.js
@@ -29,13 +29,15 @@ function Add(){
 const initial = [
     {
       id: 1,
-      title: "Todo 1",
+      title: "Todo 1 : ",
       complete: false,
+      editMode: false,
     },
     {
       id: 2,
-      title: "Todo 2",
+      title: "Todo 2 : ",
       complete: false,
+      editMode: false,
     },
 ];
 
@@ -48,20 +50,38 @@ const reducer = (state, action) => {
                 } else {
                     return todo;
             }
-        });
+            });
         case "ADD":
             //console.log(state);
             const lastTodo = state[state.length - 1];
             const newNum = lastTodo.id + 1; 
             const newTodo = {
                 id: newNum, 
-                title: "Todo " + newNum,
-                complete: false
+                title: "Todo " + newNum + " : ",
+                complete: false,
+                editMode: false,
             }
             //console.log(...state,newTodo);
             const newState = [...state,newTodo]
             return newState;   
-
+        case "EDIT":
+            console.log(action.id);
+            return state.map((todo) => {
+                if (todo.id === action.id) {
+                    return { ...todo, editMode: !todo.editMode };
+                } else {
+                    return todo;
+            }
+            });
+        case "SAVE":
+            console.log(action.id);
+            return state.map((todo) => {
+                if (todo.id === action.id) {
+                    return { ...todo, hidden: true };
+                } else {
+                    return todo;
+            }
+            });    
         default:
             return state;  
     }
@@ -73,10 +93,13 @@ const [todos, dispatch] = useReducer(reducer, initial);
 
 const handleCompleted = (todo) => { dispatch({ type: "COMPLETE", id: todo.id }); };
 const handleAdd = () => { dispatch({ type: "ADD"}); };
+const handleEdit = (todo) => { dispatch( { type: "EDIT", id: todo.id})};
+const handleSave = (todo) => { dispatch( { type: "SAVE", id: todo.id})};
 
 return(
     <>
         <h1>Claire's ToDo List</h1>
+        <div id="todoList" >
         {todos.map((todo) => (
             <div key={todo.id}>
             <label>
@@ -86,10 +109,19 @@ return(
                     onChange={() => handleCompleted(todo)}
                 />
             {todo.title}
-          </label>
+            </label>
+            <label onDoubleClick={() => handleEdit(todo)}>
+                <input id="editBox"
+                    type= "text"
+                    hidden= {!todo.editMode}
+                />
+                <button hidden={true} onClick={() => handleSave(todo)}>Save</button>
+            Click here to edit
+            </label>
         </div>
         ))}
         <button id="add" onClick={handleAdd}>+</button>
+        </div>
     </>
 );
 }
